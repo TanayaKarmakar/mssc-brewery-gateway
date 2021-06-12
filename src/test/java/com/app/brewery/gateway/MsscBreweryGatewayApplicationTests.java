49a4461348642b2ec8f9ba5cb8d5a7ead1b51906
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-06-12 09:01:04 +0530

    gateway initial setup

---
+++ src/test/java/com/app/brewery/gateway/MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.brewery.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
